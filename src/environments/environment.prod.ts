6f0e2e80648ce2d198da79d4387dc3901efe6b51
Tati <franciscoperaltafrancisco@gmail.com>
2023-03-27 10:33:56 -0300

    enviroments configurados

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   firebase: {
-    projectId: 'agenda-34772',
-    appId: '1:61041256578:web:2b231572184fe97986ec9b',
-    storageBucket: 'agenda-34772.appspot.com',
-    apiKey: 'AIzaSyC127vQvBqZRYbI0ZPMZ7DzvtkZ-5mWJd8',
-    authDomain: 'agenda-34772.firebaseapp.com',
-    messagingSenderId: '61041256578',
-    measurementId: 'G-G0YBTCM3R7',
+    apiKey: "AIzaSyC127vQvBqZRYbI0ZPMZ7DzvtkZ-5mWJd8",
+    authDomain: "agenda-34772.firebaseapp.com",
+    projectId: "agenda-34772",
+    storageBucket: "agenda-34772.appspot.com",
+    messagingSenderId: "61041256578",
+    appId: "1:61041256578:web:2b231572184fe97986ec9b",
+    measurementId: "G-G0YBTCM3R7"
   },
   production: true
 };
